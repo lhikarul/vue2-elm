f7c16972aaac1fe0d9ee3ffa9234a1887e7321e8
lhikarul <711hikaru@gmail.com>
2022-07-30 15:48:16 +0800

    feat: spped-measure/webpack-plugin, webpack-bundle-analyzer, thread-loader

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
 const path = require("path");
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+const smp = new SpeedMeasurePlugin({
+  disabled: !(process.env.MEASURE === "true"),
+});
+
 module.exports = {
-  configureWebpack: {
+  configureWebpack: smp.wrap({
     resolve: {
       alias: {
         src: path.resolve(__dirname, "./src"),
@@ -8,5 +16,22 @@ module.exports = {
         components: path.resolve(__dirname, "./src/components"),
       },
     },
-  },
+    // module: {
+    //   rules: [
+    //     {
+    //       test: /\.js$/,
+    //       exclude: "/node_modules/",
+    //       use: [
+    //         {
+    //           loader: "thread-loader",
+    //           options: {
+    //             worker: 2,
+    //           },
+    //         },
+    //       ],
+    //     },
+    //   ],
+    // },
+    plugins: [new BundleAnalyzerPlugin()],
+  }),
 };
